d514c8368f00c547d4953fcd249d34bce7235fc4
Yirsis Serrano <yirsishertz02@gmail.com>
2023-04-26 17:20:35 -0600

    ..

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,16 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from "vitest";
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import { mount } from "@vue/test-utils";
+import HelloWorld from "../HelloWorld.vue";
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+describe("HelloWorld", () => {
+  it("renders properly", () => {
+    const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } });
+    expect(wrapper.text()).toContain("Hello Vitest");
+  });
+
+  it("2+2 is 4", () => {
+    const sum = () => 2 + 2;
+    expect(sum()).toBe(5);
+  });
+});
